cb5daace36bf8c7c651216b0f58f63cb028ac32a
Akib Jawad <akibjawaad@gmail.com>
2022-02-27 23:55:59 +0600

    camera implemented

---
+++ lib/HomePage.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+import 'main.dart';
+
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+   CameraController cameraController;
+   CameraImage imgCamera;
+  bool isWorking=false;
+
+
+  initCamera()
+  {
+    cameraController = CameraController(cameras[0], ResolutionPreset.medium);
+    cameraController.initialize().then((value)
+    {
+      if(!mounted)
+        {
+          return;
+        }
+
+      setState(() {
+        cameraController.startImageStream((imageFromStream) =>
+        {
+          if(!isWorking)
+            {
+              isWorking=true,
+              imgCamera = imageFromStream,
+            }
+        });
+      });
+
+    });
+  }
+
+
+  @override
+  void initState(){
+    super.initState();
+
+    initCamera();
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    Size size=MediaQuery.of(context).size;
+    List<Widget> stackChildrenWidgets=[];
+
+    stackChildrenWidgets.add(
+        Positioned(
+          top: 0.0,
+          left: 0.0,
+          width: size.width, height: size.height-100,
+          child: Container(
+              height: size.height-100,
+
+            child: (!cameraController.value.isInitialized) ? new Container(): AspectRatio(
+                    aspectRatio: cameraController.value.aspectRatio,
+                    child: CameraPreview(cameraController),
+                 ),
+
+
+          ),
+      )
+    );
+
+
+    return SafeArea(
+                child: Scaffold(
+                  backgroundColor: Colors.black,
+                  body: Container(
+                    margin: EdgeInsets.only(top:50),
+                    color: Colors.black,
+                    child: Stack(
+                      children: stackChildrenWidgets,
+                    ),
+                  )
+                ));
+  }
+}
